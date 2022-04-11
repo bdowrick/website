f09e4e9f5a035a805fac5b10eff1bb5aaffcef8f
Byron <xenthex99@gmail.com>
2022-04-11 22:54:58 +1000

    progress on index.js

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,13 @@
 import * as React from 'react';
-import Button from '@mui/material/Button';
-import ButtonGroup from '@mui/material/ButtonGroup';
-import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography'
+import Select from '../components/select';
 
-export default function VariantButtonGroup() {
+export default function BasicTabs() {
+
   return (
-    <Box
-      sx={{
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        '& > *': {
-          m: 1,
-        },
-      }}
-    >
-      <ButtonGroup variant="outlined" aria-label="outlined button group">
-        <Button>One</Button>
-        <Button>Two</Button>
-        <Button>Three</Button>
-      </ButtonGroup>
-      <ButtonGroup variant="text" aria-label="text button group">
-        <Button>One</Button>
-        <Button>Two</Button>
-        <Button>Three</Button>
-      </ButtonGroup>
-    </Box>
+    <>
+      <Typography variant="h1" color="initial" sx={{display:'inline-block'}}>bdowrick.net</Typography>
+      <Select />
+    </>
   );
 }
